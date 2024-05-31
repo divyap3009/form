450fe4e2d27fbb23e5a5edf89444c967e9b2a933
Divya Prakash <89380519+divyap3009@users.noreply.github.com>
2024-05-31 17:11:41 +0530

    Add files via upload

---
+++ form/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FormComponent from "./components/Form";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <FormComponent />
+      </div>
+    </>
+  );
+};
+
+export default App;
